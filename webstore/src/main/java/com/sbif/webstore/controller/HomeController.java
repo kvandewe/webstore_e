3f270b105a3a6b18c09939d7f60bf573fae291b3
kvandewe <kvandewe@gmail.com>
2014-07-15 18:23:16 -0400

    Change-Id: I937b8458846a3b7c4bb23268f5b0db3f6f8784ec

--- webstore/src/main/java/com/sbif/webstore/controller/HomeController.java
+++ webstore/src/main/java/com/sbif/webstore/controller/HomeController.java
@@ -13,6 +13,7 @@ public class HomeController {
 	public String welcome(Model model) {
 		model.addAttribute("greeting", "Welcome to Web Store!");
 		model.addAttribute("tagline", "The one and only amazing webstore");
+		model.addAttribute("gitline", "SCM GitHub");
 		return "welcome";
 	}
 }
