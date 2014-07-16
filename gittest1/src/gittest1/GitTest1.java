f57dc2f961bff8fec3570f3e01d25890c2aa0cc8
kvandewe <kvandewe@gmail.com>
2014-07-16 07:59:03 -0400

    Initial commit

---
+++ gittest1/src/gittest1/GitTest1.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package gittest1;
+
+/**
+ * @author Koen
+ *
+ */
+public class GitTest1 {
+	  public static void main(String[] args) {
+		    System.out.println("Git is fun");
+		  }
+
+}
