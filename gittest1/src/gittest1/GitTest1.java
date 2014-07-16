cdb46dcb3658f3ef43410e21faa1cbc53273fa1c
kvandewe <kvandewe@gmail.com>
2014-07-16 09:22:16 -0400

    Bracnh test1

--- gittest1/src/gittest1/GitTest1.java
+++ gittest1/src/gittest1/GitTest1.java
@@ -9,7 +9,7 @@ package gittest1;
  */
 public class GitTest1 {
 	  public static void main(String[] args) {
-		    System.out.println("Git is fun - test2");
+		    System.out.println("Git is fun - - branch test 1");
 		  }
 
 }
