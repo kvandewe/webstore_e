76a348eb2c492e55982c179face414526a2c437d
kvandewe <kvandewe@gmail.com>
2014-07-22 13:54:54 -0400

    Temp check in
    Change-Id: Id46ab15732671e6ad40d84ef97edd5d07904c216

---
+++ webstore/src/main/java/com/sbif/webstore/controller/ProductController.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.sbif.webstore.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sbif.webstore.domain.Product;
+
+/**
+ * @author Koen
+ *
+ */
+@Controller
+public class ProductController {
+
+	@RequestMapping("/products")
+	public String list(Model model) {
+		Product iphone = new Product(" P1234", " iPhone 5s",
+				new BigDecimal(500));
+		iphone.setDescription(" Apple iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
+		iphone.setCategory(" Smart Phone");
+		iphone.setManufacturer(" Apple");
+		iphone.setUnitsInStock(1000);
+		model.addAttribute("product", iphone);
+
+		return "products";
+	}
+
+}
